7521fcb80a25b763d9ae38527817cb3f53e48af8
Iván Portillo Perez <ivan.portillo@unir.net>
2022-02-24 23:59:59 +0100

    maquetado footer con el formulario

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import "./Footer.css";
 import Form from "./FooterForm";
-
 const Footer = () => {
     return (
         <footer className="footer">
-            <Form/>
             <div className="footer-contact">
+                <div className="contact-description">
+                <h1>Contacto</h1>
+                    <br/> <br/>
+                    <p>Tienes algo que contarme, dejame tus datos y hablemos</p>
+                </div>
+                <div className="contact-form">
+                    <Form/>
+                </div>
             </div>
             <div className="footer-sns">
+                {/* div links */}
                 <div className="sns-links">
                     <a href="https://www.instagram.com/?hl=es" target="_blank" rel="noreferrer">
                         <i className="fab fa-instagram instagram"></i>
@@ -23,10 +30,10 @@ const Footer = () => {
                         <i className="fas fa-envelope envelope"></i>
                     </a>
                 </div>
-
+                {/* div design */}
                 <div className="design-by">
-                    <a href="https://ivan-portillo-perez.com" target="_blank" rel="noreferrer">
-                            Diseñado por Iván Portillo
+                    <a href="https://notasweb.me" target="_blank" rel="noreferrer" className="design_autor">
+                            Diseñado por Iván Portillo, creador de Notasweb
                     </a>    
                 </div>
             </div>
