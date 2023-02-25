ea5f9b9e47d33a9338bd2392f733e5fac0888f82
Iván portillo <ipp_1981@hotmail.com>
2023-02-25 18:07:59 +0100

    register

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -15,3 +15,24 @@ export async function sendMessage (dataFromForm) {
         console.log(e.status)
     }
 }
+
+
+export async function registerVisited () {
+    let previousPageUrl = document.referrer;
+
+    if ( previousPageUrl === "" ) {
+        previousPageUrl = document.URL
+    }
+
+    axios.post('https://notasweb.me/api/v1/visits', {
+        referrer: previousPageUrl
+    })
+        .then(function (response) {
+            console.log('Welcome Blue Rhino!!');
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+}
+
