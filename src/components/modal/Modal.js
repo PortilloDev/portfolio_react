b6937c206d9fe1be2020f11ec94a88e007101a02
ivan <ivan.portillo@mo2o.com>
2023-06-22 22:53:30 +0200

    modal open

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -2,8 +2,8 @@ import React from 'react'
 import './Modal.css'
 import InfoModal from '../info/InfoModal'
 export default function Modal({children, open, close}) {
-    {/* cambiar por esto if(!open) */}
-    if(open) return null
+
+  if(!open) return null
     return (
         <>
         <div className="modal-overlay">
