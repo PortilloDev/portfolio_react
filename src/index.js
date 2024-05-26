2c90ac8272de27e5877d2eb55c5635c60677ba3d
Iván portillo <ipp_1981@hotmail.com>
2024-05-26 13:25:44 +0200

    Implement caching for images and videos using Workbox

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import "./index.css";
 
+// Registrar el Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registrado: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registro fallido: ', registrationError);
+    });
+  });
+}
+
 ReactDOM.render(
     <App />,
   document.getElementById('root')
